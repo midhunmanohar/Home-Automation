f627d43d9cc84effb52fef6a1cf4323b0831329a
midhunmanohar <39940018+midhunmanohar@users.noreply.github.com>
2019-04-06 11:24:22 +0530

    Update timer.py
    Fan timer file path changes

--- timer.py
+++ timer.py
@@ -12,7 +12,7 @@ GPIO.setup(16, GPIO.OUT)
 time =  datetime.datetime.now().strftime("%M")
 
 def switchstatus():
-	with open("/home/pi/Projects/html/homeauto/switch.txt") as f:
+	with open("/home/pi/Python_Scripts/homeauto/switch.txt") as f:
 		temp=list(f)[-1]
 		temp=temp.replace("\n","")
 		global data
@@ -26,41 +26,43 @@ def switchoff():
 	data['fan']='OFF'
 	data['time']= datetime.datetime.now().strftime("%d %b %Y %H:%M")
 	a="0"
-	with open("/home/pi/Projects/html/homeauto/r1.fan.txt","r+") as fan:
-		fan.write(a)
-		fan.flush()
-		os.fsync(fan.fileno())
-	with open("/home/pi/Projects/html/homeauto/switch.txt", "w") as f:
+	with open("/home/pi/Python_Scripts/homeauto/switch.txt", "w") as f:
 		json.dump(data,f)
 		f.write("\n")
 		
 def checkreset():
 	check='0';
-	with open("/home/pi/Projects/html/homeauto/timer.txt","w") as temp:
+	with open("/home/pi/Python_Scripts/homeauto/timer.txt","w") as temp:
           temp.write(check +"\n")
           temp.flush()
           os.fsync(temp.fileno())
 
 while True:
 	hour =  int(datetime.datetime.now().strftime("%H"))
-	while hour >3 and hour <9:
+	while hour >4 and hour <9:
 		hour =  int(datetime.datetime.now().strftime("%H"))
 		time =  int(datetime.datetime.now().strftime("%H%M"))
-		with open("/home/pi/Projects/html/homeauto/timer.txt") as timer:
+		with open("/home/pi/Python_Scripts/homeauto/timer.txt") as timer:
 			check=timer.read(1)
-		if check=='1' and time ==430:
+		if check=='1' and time ==500:
 			switchoff()
 			checkreset()
-		if check=='2' and time ==500:
+		if check=='2' and time ==530:
 			switchoff()
 			checkreset()
-		if check=='3' and time ==530:
+		if check=='3' and time ==600:
 			switchoff()
 			checkreset()
-		if check=='4' and time ==600:
+		if check=='4' and time ==630:
 			switchoff()
 			checkreset()
-		if check=='5' and time ==830:
+		if check=='5' and time ==700:
+			switchoff()
+			checkreset()
+		if check=='6' and time ==830:
+			switchoff()
+			checkreset()
+		if check=='7' and time ==900:
 			switchoff()
 			checkreset()
 		sleep(60)
