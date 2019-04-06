9161c87746ddc55247332131716558adea8e768d
midhunmanohar <39940018+midhunmanohar@users.noreply.github.com>
2019-04-06 11:24:53 +0530

    Update voltage.py
    Removed while loop and scheduled via Crontab

--- voltage.py
+++ voltage.py
@@ -11,51 +11,53 @@ voltage = 0.0
 sum = 0
 sample_count = 0
 num_samples = 10
-while(1):
-   ser=serial.Serial('/dev/ttyS0',115200)
-   #text = GPIO.input(10)
-   #print (text)
-   while (sample_count < num_samples):
-       sum = sum + int(ser.readline().replace("\n",""))
-       sample_count = sample_count + 1
-   try:
-       #voltage = int(ser.readline().replace("\n",""))
-	   voltage = (float(sum)/float(num_samples)*3.93)/1023
-	   #voltage = (float(sum)/float(num_samples)*3.3)/1023
-	   #print sum
-	   #print(voltage)
-       #voltage = (3.8*voltage*(R1+R2))/(1023*R2)
-	   voltage = voltage*4.85
-	   #voltage = voltage*5.2
-   except:
-       print("Sensor down")
-   try:
-	   if voltage>12.9:
-	      #requests.get(url+'Battery Voltage update: '+'{0:0.1f}'.format(voltage))
+ser=serial.Serial('/dev/ttyS0',115200)
+#text = GPIO.input(10)
+#print (text)
+while (sample_count < num_samples):
+   sum = sum + int(ser.readline().replace("\n",""))
+   sample_count = sample_count + 1
+try:
+   #voltage = int(ser.readline().replace("\n",""))
+   #voltage = (float(sum)/float(num_samples)*3.93)/1023
+   #voltage = (float(sum)/float(num_samples)*3.3)/1023
+   voltage = (float(sum)/float(num_samples)*4.75)/1023
+   #print sum
+   print(voltage)
+   #voltage = (3.8*voltage*(R1+R2))/(1023*R2)
+   voltage = voltage*4.95
+   #voltage = voltage*5.2
+   print(voltage)
+except:
+   print("Sensor down")
+try:
+   if voltage>12.9:
+	  #print("")
+	  #requests.get(url+'Battery Voltage update: '+'{0:0.1f}'.format(voltage))
 		  requests.get(url+'Battery Charge: 100% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   if voltage>12.7 and voltage <12.9:
-		  requests.get(url+'Battery Charge: 100% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   if voltage>12.5 and voltage <12.7:
-		  requests.get(url+'Battery Charge: 90% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   if voltage>12.4 and voltage <12.5:
-		  requests.get(url+'Battery Charge: 80% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   if voltage>12.3 and voltage <12.4:
-		  requests.get(url+'Battery Charge: 70% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   if voltage>12.2 and voltage <12.3:
-		  requests.get(url+'Battery Charge: 60% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   if voltage>12.0 and voltage <12.2:
-		  requests.get(url+'Battery Charge: 50% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   if voltage>11.9 and voltage <12.0:
-		  requests.get(url+'Battery Charge: 40% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   if voltage>11.7 and voltage <11.9:
-		  requests.get(url+'Battery Charge: 30% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   if voltage <11.7:
-		  requests.get(url+'Battery Charge Critical: 20% \n'+'{0:0.1f}'.format(voltage)+'V')
-	   #print ('{0:0.1f}'.format(voltage))
-   except:
-       print("Connection error... retrying...")
-   #print (ser.readline())
-   
-   sum = 0
-   sample_count = 0
-   sleep(900)
+   if voltage>12.7 and voltage <12.9:
+	  requests.get(url+'Battery Charge: 100% \n'+'{0:0.1f}'.format(voltage)+'V')
+   if voltage>12.5 and voltage <12.7:
+	  requests.get(url+'Battery Charge: 90% \n'+'{0:0.1f}'.format(voltage)+'V')
+   if voltage>12.4 and voltage <12.5:
+	  requests.get(url+'Battery Charge: 80% \n'+'{0:0.1f}'.format(voltage)+'V')
+   if voltage>12.3 and voltage <12.4:
+	  requests.get(url+'Battery Charge: 70% \n'+'{0:0.1f}'.format(voltage)+'V')
+   if voltage>12.2 and voltage <12.3:
+	  requests.get(url+'Battery Charge: 60% \n'+'{0:0.1f}'.format(voltage)+'V')
+   if voltage>12.0 and voltage <12.2:
+	  requests.get(url+'Battery Charge: 50% \n'+'{0:0.1f}'.format(voltage)+'V')
+   if voltage>11.9 and voltage <12.0:
+	  requests.get(url+'Battery Charge: 40% \n'+'{0:0.1f}'.format(voltage)+'V')
+   if voltage>11.7 and voltage <11.9:
+	  requests.get(url+'Battery Charge: 30% \n'+'{0:0.1f}'.format(voltage)+'V')
+   if voltage <11.7:
+	  requests.get(url+'Battery Charge Critical: 20% \n'+'{0:0.1f}'.format(voltage)+'V')
+   print ('{0:0.1f}'.format(voltage))
+except:
+   print("Connection error... retrying...")
+#print (ser.readline())
+
+sum = 0
+sample_count = 0
+#sleep(30)
