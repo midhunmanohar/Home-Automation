7508d1df6816a848739776804e1bfa3fe4109f70
midhunmanohar <39940018+midhunmanohar@users.noreply.github.com>
2019-04-06 11:20:39 +0530

    Launcher shell script changes
    Moved scheduling to Crontab

--- launcher.sh
+++ launcher.sh
@@ -6,15 +6,16 @@ cd
 cd /home/pi/Python_Scripts/homeauto
 python /home/pi/Python_Scripts/homeauto/homeauto.py &
 sudo python /home/pi/Python_Scripts/homeauto/switch.py &
-#sudo python /home/pi/Projects/html/Navratri/navaratri.py &
 sleep 2
-sudo python /home/pi/Python_Scripts/homeauto/temperature.py &
+#sudo python /home/pi/Python_Scripts/homeauto/temperature.py &
 sleep 10
 sudo python /home/pi/Python_Scripts/homeauto/telegramtext.py &
-#sudo python /home/pi/Python_Scripts/homeauto/telegramfantimer.py &
-#sudo python /home/pi/Python_Scripts/homeauto/timer.py &
-#sudo python /home/pi/Projects/html/homeauto/PIR.py &
+sudo python /home/pi/Python_Scripts/homeauto/telegramfantimer.py &
+sudo python /home/pi/Python_Scripts/homeauto/timer.py &
 sudo python /home/pi/Python_Scripts/homeauto/CPUtempmon.py &
 sudo python /home/pi/Python_Scripts/homeauto/adafruitmqtt.py &
-#sudo python /home/pi/Python_Scripts/homeauto/voltage.py &
+#python /home/pi/Python_Scripts/homeauto/voltage.py &
+#python /home/pi/Python_Scripts/homeauto/awstempsensor.py &
+#sleep 2
+#python /home/pi/Python_Scripts/homeauto/awsvoltage.py &
 cd
